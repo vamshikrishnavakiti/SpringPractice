7eae3f23dbc8f19cfaedc2d948ca52007cc1d2bc
Vamshi Krishna <vamshi.qaeng@gmail.com>
2023-09-19 10:14:53 +0530

    Created bean Package

---
+++ src/com/sp/beans/student.java
@@ -0,0 +1,28 @@
+package com.sp.beans;
+
+public class student {
+	private String name;
+	private int rollNum;
+	private String email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollNum() {
+		return rollNum;
+	}
+	public void setRollNum(int rollNum) {
+		this.rollNum = rollNum;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
